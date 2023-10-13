1aee6abf9459b0674eb0f268b879b42f11ee2537
salman89dev <hmsalmansadiq@gmail.com>
2023-10-13 20:35:23 +0500

    both assignment

--- lib/main.dart
+++ lib/main.dart
@@ -1,91 +1,234 @@
 import 'package:flutter/material.dart';
 
-void main() {
+void main()
+{
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context)
+  {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Periodic Table'),
+          title: Text("Chess Board"),
+          centerTitle: true,
         ),
-        body: PeriodicTable(),
+        body: SingleChildScrollView(
+          child:Column(
+            children: [
+
+              Row(
+                children: [
+                  Container(
+                    height: 200,
+                    width: 213,
+                    color: Colors.black,
+                  ),
+                  Container(
+                    height: 200,
+                    width: 213,
+                    color: Colors.white,
+                  ),
+                  Container(
+                    height: 200,
+                    width: 213,
+                    color: Colors.black,
+                  ),
+                  Container(
+                    height: 200,
+                    width: 213,
+                    color: Colors.white,
+                  ),
+                  Container(
+                    height: 200,
+                    width: 213,
+                    color: Colors.black,
+                  ),
+                  Container(
+                    height: 200,
+                    width: 213,
+                    color: Colors.white,
+                  ),
+                ],
+              ),
+              
+              Row(
+                children: [
+                  Container(
+                    height: 200,
+                    width: 213,
+                    color: Colors.white,
+                  ),
+                  Container(
+                    height: 200,
+                    width: 213,
+                    color: Colors.black,
+                  ),
+                  Container(
+                    height: 200,
+                    width: 213,
+                    color: Colors.white,
+                  ),
+                  Container(
+                    height: 200,
+                    width: 213,
+                    color: Colors.black,
+                  ),
+                  Container(
+                    height: 200,
+                    width: 213,
+                    color: Colors.white,
+                  ),
+                  Container(
+                    height: 200,
+                    width: 213,
+                    color: Colors.black,
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Container(
+                    height: 200,
+                    width: 213,
+                    color: Colors.black,
+                  ),
+                  Container(
+                    height: 200,
+                    width: 213,
+                    color: Colors.white,
+                  ),
+                  Container(
+                    height: 200,
+                    width: 213,
+                    color: Colors.black,
+                  ),
+                  Container(
+                    height: 200,
+                    width: 213,
+                    color: Colors.white,
+                  ),
+                  Container(
+                    height: 200,
+                    width: 213,
+                    color: Colors.black,
+                  ),
+                  Container(
+                    height: 200,
+                    width: 213,
+                    color: Colors.white,
+                  ),
+                ],
+              ),
+            ],
+          )
+           ),
       ),
     );
   }
+
 }
 
-class PeriodicTable extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                ElementCell(symbol: 'H', name: 'Hydrogen'),
-                ElementCell(symbol: 'He', name: 'Helium'),
-                ElementCell(symbol: 'Li', name: 'Lithium'),
-                ElementCell(symbol: 'Be', name: 'Beryllium'),
-                ElementCell(symbol: 'B', name: 'Boron'),
-                ElementCell(symbol: 'C', name: 'Carbon'),
-                ElementCell(symbol: 'N', name: 'Nitrogen'),
-                ElementCell(symbol: 'O', name: 'Oxygen'),
-                ElementCell(symbol: 'F', name: 'Fluorine'),
-                ElementCell(symbol: 'Ne', name: 'Neon'),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                // Add more ElementCell widgets for the second row of elements
-              ],
-            ),
-            // Add more rows of ElementCell widgets for the rest of the periodic table
-          ],
-        ),
-      ),
-    );
-  }
-}
 
-class ElementCell extends StatelessWidget {
-  final String symbol;
-  final String name;
 
-  ElementCell({required this.symbol, required this.name});
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 50.0,
-      height: 50.0,
-      margin: EdgeInsets.all(2.0),
-      decoration: BoxDecoration(
-        color: Colors.lightBlue,
-        border: Border.all(),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            symbol,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 14.0,
-            ),
-          ),
-          SizedBox(height: 4.0),
-          Text(
-            name,
-            style: TextStyle(fontSize: 10.0),
-          ),
-        ],
-      ),
-    );
-  }
-}
+
+// PERIODIC TABLE
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Periodic Table'),
+//         ),
+//         body: PeriodicTable(),
+//       ),
+//     );
+//   }
+// }
+
+// class PeriodicTable extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: SingleChildScrollView(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             Row(
+//               children: <Widget>[
+//                 ElementCell(symbol: 'H', name: 'Hydrogen'),
+//                 ElementCell(symbol: 'He', name: 'Helium'),
+//                 ElementCell(symbol: 'Li', name: 'Lithium'),
+//                 ElementCell(symbol: 'Be', name: 'Beryllium'),
+//                 ElementCell(symbol: 'B', name: 'Boron'),
+//                 ElementCell(symbol: 'C', name: 'Carbon'),
+//                 ElementCell(symbol: 'N', name: 'Nitrogen'),
+//                 ElementCell(symbol: 'O', name: 'Oxygen'),
+//                 ElementCell(symbol: 'F', name: 'Fluorine'),
+//                 ElementCell(symbol: 'Ne', name: 'Neon'),
+//               ],
+//             ),
+//             Row(
+//               children: <Widget>[
+//                 // Add more ElementCell widgets for the second row of elements
+//               ],
+//             ),
+//             // Add more rows of ElementCell widgets for the rest of the periodic table
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// class ElementCell extends StatelessWidget {
+//   final String symbol;
+//   final String name;
+
+//   ElementCell({required this.symbol, required this.name});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       width: 50.0,
+//       height: 50.0,
+//       margin: EdgeInsets.all(2.0),
+//       decoration: BoxDecoration(
+//         color: Colors.lightBlue,
+//         border: Border.all(),
+//       ),
+//       child: Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: <Widget>[
+//           Text(
+//             symbol,
+//             style: TextStyle(
+//               fontWeight: FontWeight.bold,
+//               fontSize: 14.0,
+//             ),
+//           ),
+//           SizedBox(height: 4.0),
+//           Text(
+//             name,
+//             style: TextStyle(fontSize: 10.0),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
